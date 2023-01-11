14c479d5e56ccbbaa53751d8f23e7d16241264ef
Juntao Qiu <juntao.qiu@gmail.com>
2023-01-11 17:11:23 +1100

    updated a few slides

---
+++ components/Payment.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import {useRoundUp} from "./useRoundUp";
+
+function formatMessage(agreeToDonate: boolean, tip: number) {
+  return agreeToDonate ? 'Thanks for your donation.' : `I would like to donate $${tip} to charity.`;
+}
+
+export const Payment = ({amount}: { amount: number }) => {
+  const [agreeToDonate, setAgreeToDonate] = useState<boolean>(false);
+  const {total, tip} = useRoundUp(amount, agreeToDonate);
+
+  const handleChange = () => {
+    setAgreeToDonate(agreeToDonate => !agreeToDonate)
+  }
+
+  return <div className="container">
+    <h3>Payment</h3>
+    <label>
+      <input type="checkbox" onChange={handleChange} checked={agreeToDonate}/>
+      <p>{formatMessage(agreeToDonate, tip)}</p>
+    </label>
+    <button>${total}</button>
+  </div>
+}
